726cc256c3dfc0673f8e346a456f2c749bdbdf37
chorna111 <achorna114@gmail.com>
2022-11-21 13:29:21 +0100

    zadania after class

--- 06-Arrays/44.py
+++ 06-Arrays/44.py
@@ -1,6 +1,12 @@
 def transpose(m):
+    try:
+        result = [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
+    except:
+        result=[]
+        for i in m:
+            result.append(i)
+
     
-    result = [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
     
     return result
 for i in transpose([[1,2,3],[4,5,6],[7,8,9]]):
@@ -9,6 +15,10 @@ for i in transpose([[1,2,3],[4,5,6],[7,8,9]]):
 for i in transpose([[1,2,3,4,5],[6,7,8,9,0]]):
     print(i)
     print()
+for i in transpose([1,2,3,4,5]):
+    print([i])
+    print()
+
 
 
 
