6082e27e6b5bb7a333550d7d22a6fec2b199521c
chorna111 <achorna114@gmail.com>
2022-12-05 12:02:02 +0100

    zad 5 mock zmiany

--- 09-Test2/mock5.py
+++ 09-Test2/mock5.py
@@ -5,17 +5,8 @@ def f(first_letter,last_letter):
     d=open('/Users/onostasia/Downloads/test.txt','r')
     text=d.read()
     d.close()
-    letters=re.findall(fr"\b{first_letter}\w+{last_letter}\b",text)
-    total=0
-    for i in letters:
-        total+=1
-   
-   
-   
-        
-
-    
-    
-    return total
+    letters=re.findall(r"\b%s\w+%s\b"%(first_letter,last_letter),text)
+    return len(letters)
 print(f('w','d'))
+
 
